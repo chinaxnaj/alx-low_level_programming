6f2bd1888436304ee3ddf517e3e28bdd896bdf09
chinaxnaj <123200706+chinaxnaj@users.noreply.github.com>
2023-03-02 18:04:27 +0100

    new

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,55 +1,40 @@
 #include "main.h"
 
 /**
-
- * cap_string - capitalizes all words of a string
-
- * @s: input string.
-
- * Return: the pointer to dest.
-
+ * cap_string - capitalizes all words of a string.
+ * @s: string to use.
+ *
+ * Return: string.
  */
 
 char *cap_string(char *s)
-
 {
+	int i = 1, j, check;
+	char a[] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}', '\n', '\t', ' '};
 
-	int count = 0, i;
+	if (s[0] > 96 && s[0] < 123)
+		s[0] -= 32;
 
-	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
-
-	if (*(s + count) >= 97 && *(s + count) <= 122)
-
-		*(s + count) = *(s + count) - 32;
-
-	count++;
-
-	while (*(s + count) != '\0')
-
+	while (s[i] != '\0')
 	{
-
-		for (i = 0; i < 13; i++)
-
+		if (s[i] > 96 && s[i] < 123)
 		{
-
-			if (*(s + count) == sep_words[i])
-
+			j = 0;
+			check = 0;
+			while (check == 0 && j < 13)
 			{
-
-				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
-
-					*(s + (count + 1)) = *(s + (count + 1)) - 32;
-
-				break;
-
+				if (s[i - 1] == a[j])
+				{
+					check = 1;
+				}
+				j++;
 			}
-
+			if (check == 1)
+			{
+				s[i] -= 32;
+			}
 		}
-
-		count++;
-
+		i++;
 	}
-
 	return (s);
-
 }
