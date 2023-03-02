52c5531b0f21971fb5066193fd9aa0bc6482a990
chinaxnaj <123200706+chinaxnaj@users.noreply.github.com>
2023-03-02 11:44:54 +0100

    Changes

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,32 +1,58 @@
 #include "main.h"
 
 /**
- * *_strcat - function commute srtings
- * @dest: param pointer to a char
- * @src: param pointer to a char
- * Return: return value of dest
+
+ * _strcat - concatenates two strings together,
+
+ * @dest: destination.
+
+ * @src: source.
+
+ * Return: the pointer to dest.
+
  */
 
 char *_strcat(char *dest, char *src)
+
 {
-	int i;
-	int j;
 
-	i = 0;
-	j = 0;
+	int count = 0, count2 = 0;
 
-	while (dest[i] != '\0')
+	while (*(dest + count) != '\0')
+
 	{
-		i++;
+
+		count++;
+
 	}
 
-	while (src[j] != '\0')
+	while (count2 >= 0)
+
 	{
-		dest[i] = src[j];
-		j++;
-		i++;
+
+		*(dest + count) = *(src + count2);
+
+		if (*(src + count2) == '\0')
+
+			break;
+
+		count++;
+
+		count2++;
+
 	}
 
-	dest[i] = '\0';
 	return (dest);
+
 }
+
+
+
+
+	
+
+	
+	
+
+
+
