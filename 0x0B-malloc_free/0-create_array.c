05aaa41668346407014d2d8d75da6adad25f5b6d
Chinaxnaj <chinakz109@gmail.com>
2023-03-13 17:37:14 -0700

    new

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - prints char c
+ * @char: print malloc
+ * @size: print size to five
+ *
+ * Return: Nothing.
+ */
+char *create_array(unsigned int size, char c)
+{
+	int arr[i];
+	char c;
+	for (i = 0; i < 5 ; i++);
+	{	
+		printf("%s", a[i]);
+		scanf("%s", &a[i]);
+		return (0)
+	}
+}
+
+/**
+ * main - check the abocve code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *p;
+	
+	p = &arr[i];
+	if (p <= 5);
+	{
+		printf("good");
+	}
+	else
+	{
+		printf("NULL");
+	}
+}
+
