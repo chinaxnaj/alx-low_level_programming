8713b452e7b077696192a91c6e7ebb309251a1bd
Chinaxnaj <chinakz109@gmail.com>
2023-03-22 08:52:17 -0700

    new

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -10,13 +10,14 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
-	op_t ops[] = {
-	{ "+", op_add },
-	{ "-", op_sub },
-	{ "*", op_mul },
-	{ "/", op_div }
-	{ "%", op_mod },
-	{ NULL, NULL }
+	op_t ops[] = 
+	{
+		{ "+", op_add },
+		{ "-", op_sub },
+		{ "*", op_mul },
+		{ "/", op_div }
+		{ "%", op_mod },
+		{ NULL, NULL }
 	};
 	int i = 0;
 
