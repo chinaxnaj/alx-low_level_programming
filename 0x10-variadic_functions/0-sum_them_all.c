2b5154dc3f573db99ec7f23fb14eb33e30e489b8
Chinaxnaj <chinakz109@gmail.com>
2023-03-23 11:23:52 -0700

    new

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -24,18 +24,3 @@ int sum_them_all(const unsigned int n, ...)
 	}
 	return (sum);
 }
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int sum;
-
-    sum = sum_them_all(2, 98, 1024);
-    printf("%d\n", sum);
-    sum = sum_them_all(4, 98, 1024, 402, -1024);
-    printf("%d\n", sum);
-    return (0);
-}
