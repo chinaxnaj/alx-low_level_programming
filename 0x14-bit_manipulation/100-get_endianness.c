06aba70ca3a2dbba097f09d892ca135121b04c9d
Chinaxnaj <chinakz109@gmail.com>
2023-04-08 07:47:29 -0700

    new

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *c = (char *) &i;
+
+	return (*c);
+}
+
+int main(void)
+{
+    int n;
+
+    n = get_endianness();
+    if (n != 0)
+    {
+        printf("Little Endian\n");
+    }
+    else
+    {
+        printf("Big Endian\n");
+    }
+    return (0);
+}
