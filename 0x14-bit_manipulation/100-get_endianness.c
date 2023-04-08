1d488aa7517948806b300ebecc1686e4b28f9dee
Chinaxnaj <chinakz109@gmail.com>
2023-04-08 07:51:07 -0700

    new

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -11,19 +11,3 @@ int get_endianness(void)
 
 	return (*c);
 }
-
-int main(void)
-{
-    int n;
-
-    n = get_endianness();
-    if (n != 0)
-    {
-        printf("Little Endian\n");
-    }
-    else
-    {
-        printf("Big Endian\n");
-    }
-    return (0);
-}
