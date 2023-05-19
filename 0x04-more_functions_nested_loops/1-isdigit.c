e537961315e27047b8ab34acf4e3bf3fcdb363f9
Chinaxnaj <chinakz109@gmail.com>
2023-05-19 15:18:26 -0700

    new

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -15,4 +16,20 @@ int _isdigit(int c)
 	{
 		return (0);
 	}
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char c;
+
+    c = '0';
+    printf("%c: %d\n", c, _isdigit(c));
+    c = 'a';
+    printf("%c: %d\n", c, _isdigit(c));
+    return (0);
 }
